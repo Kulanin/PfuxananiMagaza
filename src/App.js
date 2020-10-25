ee88f9910b6cdf7b4142aea32cb3fccf85ff6d63
Kulani Ngobeni <kulanin@net1.com>
2020-10-25 20:59:25 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Kulani by creating a stokvel application
         </a>
       </header>
     </div>
