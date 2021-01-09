ef8510859bd0f2ad85ebea082daa4df1f42cb63a
Kulani Ngobeni <kulanin@net1.com>
2021-01-09 13:13:46 +0200

    hacking

--- Back_End/Back_End.js
+++ Back_End/Back_End.js
@@ -659,6 +659,16 @@ const PORT_Number = process.env.PORT || 5001;
 //     app.use(express.static("/build"));
 // }
 
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    });
+  }
+  
+
 const server = app.listen(PORT_Number, () => {
 
     let host = server.address().address;
